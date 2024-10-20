8be0a88336aadb940f889ace89c7fc540a35cf7b
carferrio <carferrio@outlook.com>
2024-10-20 18:36:36 +0100

    little fix

--- catalog/language/en-gb/product/review.php
+++ catalog/language/en-gb/product/review.php
@@ -11,6 +11,7 @@ $_['text_success']    = 'Thank you for your review. It has been submitted to the
 $_['entry_name']       = 'Your Name';
 $_['entry_review']     = 'Your Review';
 $_['entry_rating']     = 'Rating';
+$_['entry_valueForMoney']     = 'Value for Money';
 $_['entry_good']       = 'Good';
 $_['entry_bad']        = 'Bad';
 
@@ -23,5 +24,6 @@ $_['error_product']    = 'Warning: Product could not be found!';
 $_['error_name']       = 'Review Name must be between 3 and 25 characters!';
 $_['error_text']       = 'Review Text must be between 25 and 1000 characters!';
 $_['error_rating']     = 'Please select a review rating!';
+$_['error_valueForMoney']     = 'Please select a review value for money!';
 $_['error_guest']      = 'You must login to review the product!';
 $_['error_purchased']  = 'You must have purchased this product before you can write a review!';
